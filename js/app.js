4cf53274c131d41c0e37d1560b6189058c3355f7
Matt Wujek <mattwujek@gmail.com>
2016-02-04 10:16:34 -0800

    finished scripts for table logic. need to finish the rest of the slices in illustrator.

--- js/app.js
+++ js/app.js
@@ -14,9 +14,9 @@ $(document).ready(function(){
 
   function loadTable (){
     var rowContainer = $('.row-container ');
-    var eventCount = 20;
-    var itemCount = 20;
-    var clusterCount = 20;
+    var eventCount = 40;
+    var itemCount = 15;
+    var clusterCount = 12;
 
     for (var i = 0; i < eventCount; i++) {
 
@@ -25,8 +25,8 @@ $(document).ready(function(){
       // make wrapper and container
       var eventWrapper = $('<div class="event-wrapper"></div>').appendTo(rowContainer);
       var eventContainer = $('<div class="event-container"></div>').appendTo(eventWrapper);
-      var itemContainer = $('<div class="item-container"></div>').appendTo(eventWrapper);
-      var clusterContainer = $('<div class="cluster-container"></div>').appendTo(eventWrapper);
+      var itemContainer = $('<div class="item-container hide-section"></div>').appendTo(eventWrapper);
+      var clusterContainer = $('<div class="cluster-container hide-section"></div>').appendTo(eventWrapper);
       
       // add event header
       eventContainer.append('<img src="' + eventFileName + '">');
@@ -38,29 +38,25 @@ $(document).ready(function(){
       eventContainer.append('<i class="icon ion-android-add-circle toggle-clusters"></i>');
       
 
-      // // add item rows
-      // for (var j = 0; j < itemCount; j++) {
-      //   var randNum2 = Math.floor(Math.random() * 10);
-      //   var itemFileName = "img/items/i-" + randNum2 + ".png";
-      //   itemContainer.append('<img src="' + itemFileName + '">');
-      // } // end item loop
+      // add item rows
+      for (var j = 0; j < itemCount; j++) {
+        var randNum2 = Math.floor(Math.random() * 10);
+        var itemFileName = "img/items/i-" + randNum2 + ".png";
+        itemContainer.append('<img src="' + itemFileName + '">');
+      } // end item loop
 
-      // // add cluster rows
-      // for (var k = 0; k < clusterCount; k++) {
-      //   var randNum3 = Math.floor(Math.random() * 10);
-      //   var clusterFileName = "img/clusters/c-" + randNum3 + ".png";
-      //   clusterContainer.append('<img src="' + clusterFileName + '">');
-      // } // end cluster loop
+      // add cluster rows
+      for (var k = 0; k < clusterCount; k++) {
+        var randNum3 = Math.floor(Math.random() * 10);
+        var clusterFileName = "img/clusters/c-" + randNum3 + ".png";
+        clusterContainer.append('<img src="' + clusterFileName + '">');
+      } // end cluster loop
 
 
     } // end of loop
   } // end of function
 
   loadTable();
-
-  $('.toggle-clusters').click(function() {
-    $('.table-container').find('.cluster-container').toggleClass('hide-section');
-  });
 
   // scroll function
   $('.table-wrapper').on('scroll',function(event){
@@ -78,9 +74,58 @@ $(document).ready(function(){
   });
 
   // click to expand selection
-  $('.row-container').delegate('.ion-android-add-circle', 'click', function() {
-    $(this).toggleClass('showing-children');
+  $('.row-container').delegate('.ion-android-add-circle.toggle-clusters', 'click', function() {
+
+    
+    var parentContainer = $(this).parents('.event-wrapper');
+    var itemWrapper = parentContainer.find('.item-container');
+    var clusterWrapper = parentContainer.find('.cluster-container');
+    var otherIcon = parentContainer.find('.ion-android-add-circle.toggle-items');
+
+    if ( $(this).hasClass('showing-children') ){
+
+      clusterWrapper.addClass('hide-section');
+      $(this).removeClass('showing-children');
+
+    } else {
+      clusterWrapper.removeClass('hide-section');
+      $(this).addClass('showing-children');
+      parentContainer.addClass('clusters-open')
+      if( parentContainer.hasClass('items-open') ){
+        itemWrapper.addClass('hide-section');
+        otherIcon.removeClass('showing-children');
+      }
+    }
+
+    
   });
 
+
+    // click to expand selection
+  $('.row-container').delegate('.ion-android-add-circle.toggle-items', 'click', function() {
+    
+    var parentContainer = $(this).parents('.event-wrapper');
+    var itemWrapper = parentContainer.find('.item-container');
+    var clusterWrapper = parentContainer.find('.cluster-container');
+    var otherIcon = parentContainer.find('.ion-android-add-circle.toggle-clusters');
+
+    // add classes
+    if ( $(this).hasClass('showing-children') ){
+      itemWrapper.addClass('hide-section');
+      $(this).removeClass('showing-children');
+      console.log('log');
+    } else {
+      console.log('asd');
+      itemWrapper.removeClass('hide-section');
+      $(this).addClass('showing-children');
+      parentContainer.addClass('items-open')
+      if( parentContainer.hasClass('clusters-open') ){
+        clusterWrapper.addClass('hide-section');
+        otherIcon.removeClass('showing-children');
+      }
+    }
+    
+    
+  });
   
 });
